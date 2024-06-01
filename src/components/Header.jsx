382c7997866d6cf03e288a41f95c74c844194dac
Rob Dunn <dunnrob@hotmail.co.uk>
2024-06-01 20:16:08 +0100

    updated br tags in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,10 @@ const Header = () => (
   <div className='container'>
     <h1>AEM ID Assistant</h1>
     <p className='description'>
-      <strong>ID to Test: </strong> 
-     
+      <strong>ID to Test: </strong>
       core-leads/hal-retail/lins/app/asm/s/tn/rn/cnv/halappasm-test-lead-tile-0224gener1
       <br />
       <strong>API test ID: </strong>
-  
       core-leads/lly-retail/card/app/asm/s/tn/rn/cnv/llyappasm-card-contextual-22droploo1
       <br /> <br />
       Experiment by altering a segment, such as replacing 'dkp' or 'hal' with
@@ -20,25 +18,21 @@ const Header = () => (
       <br /> <br />
       For API interactions, please conduct tests within a development Chrome
       instance due to CORS restrictions. Follow these steps to initiate a
-      session with disabled security:<br /><br />
-      
-       
-          On Windows: Press the <em>Windows key + R</em> and run the script:{' '}
-          <strong>
-            "chrome.exe --user-data-dir='C://Chrome dev session'
-            --disable-web-security"
-          </strong><br />
-
-       
-       
-          On Mac: Enter the following into the terminal:{' '}
-          <strong>
-            open -na "Google Chrome" --args --user-data-dir='/tmp/Chrome dev
-            session' --disable-web-security"
-          </strong>
-          .
-       
-      
+      session with disabled security:
+      <br />
+      <br />
+      On Windows: Press the <em>Windows key + R</em> and run the script:{' '}
+      <strong>
+        "chrome.exe --user-data-dir='C://Chrome dev session'
+        --disable-web-security"
+      </strong>
+      <br />
+      On Mac: Enter the following into the terminal:{' '}
+      <strong>
+        open -na "Google Chrome" --args --user-data-dir='/tmp/Chrome dev
+        session' --disable-web-security"
+      </strong>{' '}
+      <br /> <br />
       <strong>Note:</strong> This method should not be used for regular web
       browsing.
     </p>
